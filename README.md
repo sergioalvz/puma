22117e988b86b5ad23c7aed9059ea3c26246b01d
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2013-09-07 13:28:44 +0200

    Updated README file and added Main.scala

---
+++ README.md
@@ -0,0 +1,4 @@
+puma
+====
+
+The **puma** project is an Open Source project for text analyzing large collection of tweets.
---
+++ src/org/puma/main/Main.scala
@@ -0,0 +1,14 @@
+package org.puma.main
+
+/**
+ * Project: puma
+ * Package: org.puma.main
+ *
+ * User: sergioalvarez
+ * Date: 09/2013
+ */
+object Main {
+  def main (args: Array[String]) : Unit = {
+    println("Hi puma!")
+  }
+}
