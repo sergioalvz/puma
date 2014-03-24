d76506f757dc35f789cbc96a0c32c4cca0189dbb
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2014-03-24 22:51:09 +0100

    Updated ScoreGenerator for using Pulling parser

--- src/org/puma/generator/ScoreGenerator.scala
+++ src/org/puma/generator/ScoreGenerator.scala
@@ -1,12 +1,12 @@
 package org.puma.generator
 
 import org.puma.configuration.ConfigurationUtil
-import scala.xml.XML
 import org.puma.analyzer.filter._
 import scala.collection.mutable
 import scala.io.Source
 import org.puma.analyzer.NgramExtractor
 import java.io.{FileOutputStream, OutputStreamWriter, BufferedWriter, File}
+import scala.xml.pull.{EvElemEnd, EvText, EvElemStart, XMLEventReader}
 
 
 /**
@@ -20,40 +20,73 @@ class ScoreGenerator extends Generator{
 
   private[this] val resultFiles   = ConfigurationUtil.getLLRResultFiles
   private[this] val fileToAnalyze = ConfigurationUtil.getFileToAnalyze
+  private[this] val scoreByTerm   = getScores.withDefaultValue(0.0)
 
   def generate(): Unit = {
-    val scoreByTerm = getScores.withDefaultValue(0.0)
+    val location  = new mutable.StringBuilder()
+    val text      = new mutable.StringBuilder()
+    val latitude  = new mutable.StringBuilder()
+    val longitude = new mutable.StringBuilder()
 
-    val root = XML.load(s"${ConfigurationUtil.getFilesToAnalyzeAbsolutePath}$fileToAnalyze")
-    (root \\ "tweet").foreach(node => {
-      val text      = (node \\ "text")(0).text
-      val location  = (node \\ "location")(0).text
-      val longitude = (node \\ "latitude")(0).text
-      val latitude  = (node \\ "longitude")(0).text
-
-      val locationKeywords = new LocationFilter().extract(location)
-      val mentionsHashtags = new MentionFilter(new HashtagFilter()).extract(text)
-      val bigrams  = new BigramsFilter().extract(text)
-      val keywords = new KeywordFilter().extract(text).filter(keyword => {
-        bigrams.find(bigram => bigram.contains(keyword)) == None
-      })
-
-      val terms = locationKeywords ++ mentionsHashtags ++ bigrams ++ keywords
-
-      var score: Double = 0.0
-      terms.foreach(term => {
-        val format = term.mkString(" ")
-        score += scoreByTerm(format)
-      })
-      saveTweetScore(score, latitude, longitude, text)
+    var insideLocation  = false
+    var insideText      = false
+    var insideLatitude  = false
+    var insideLongitude = false
+    
+    val reader = new XMLEventReader(Source.fromFile(ConfigurationUtil.getFilesToAnalyzeAbsolutePath + fileToAnalyze))
+    reader.foreach(event => {
+      event match {
+        case EvElemStart(_, "location", _, _)  => insideLocation = true
+        case EvElemEnd(_, "location")          => insideLocation = false
+        case EvElemStart(_, "latitude", _, _)  => insideLatitude = true
+        case EvElemEnd(_, "latitude")          => insideLatitude = false
+        case EvElemStart(_, "longitude", _, _) => insideLongitude = true
+        case EvElemEnd(_, "longitude")         => insideLongitude = false
+        case EvElemStart(_, "text", _, _)      => insideText = true
+        case EvElemEnd(_, "text")              => insideText = false
+        case EvText(nodeText) =>
+        {
+          if(insideLocation)
+            location ++= nodeText
+          else if(insideLatitude)
+            latitude ++= nodeText
+          else if(insideLongitude)
+            longitude ++= nodeText
+          else if(insideText)
+            text ++= nodeText
+        }
+        case EvElemEnd(_, "tweet") =>
+        {
+          processXmlData(location.toString(), latitude.toString(), longitude.toString(), text.toString())
+          location.clear()
+          latitude.clear()
+          longitude.clear()
+          text.clear()
+        }
+        case _ => ;
+      }
     })
+  }
+
+  private[this] def processXmlData(location: String, latitude: String, longitude: String, text: String): Unit = {
+    val locationKeywords = new LocationFilter().extract(location)
+    val mentionsHashtags = new MentionFilter(new HashtagFilter()).extract(text)
+    val bigrams  = new BigramsFilter().extract(text)
+    val keywords = new KeywordFilter().extract(text).filter(keyword => {
+      bigrams.find(bigram => bigram.contains(keyword)) == None
+    })
+
+    val terms = locationKeywords ++ mentionsHashtags ++ bigrams ++ keywords
+
+    val score = terms.foldLeft(0.0)((acc, current) => acc + scoreByTerm(current.mkString(" ")))
+    saveTweetScore(score, longitude, latitude, text)
   }
 
   private[this] def saveTweetScore(score: Double, latitude: String, longitude: String, text: String) = {
     val dir  = new File(ConfigurationUtil.getOutputFilesDirAbsolutePath)
     dir.mkdirs()
 
-    val file = new File(s"${dir.getAbsolutePath}/${fileToAnalyze}_scores.tsv")
+    val file = new File(dir.getAbsolutePath + "/" + s"${fileToAnalyze}_scores.tsv")
     if(!file.exists) file.createNewFile
 
     val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true)))
@@ -75,8 +108,8 @@ class ScoreGenerator extends Generator{
     resultFiles.foreach(file => {
       val lines = Source.fromFile(ConfigurationUtil.getOutputFilesDirAbsolutePath + file).getLines().toArray
       lines.foreach(line => {
-       val pieces = line.split("\t")
-       scores(pieces(1)) = pieces(0).toDouble
+       val fields = line.split("\t")
+       scores(fields(1)) = fields(0).toDouble
       })
     })
     scores.toMap
